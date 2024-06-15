97da263b3d74aab3af9100d2f3f8f8d12ddd5c3f
Timur Zheksimbaev <t.zheksimbaev@innopolis.university>
2024-06-15 23:04:05 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -41,7 +41,6 @@ func main() {
 	// 	WriteTimeout:   10 * time.Second,
 	// 	MaxHeaderBytes: 1 << 20,
 	// }
-
-	utils.LogExit(router.Run(appConfig.ServerHost+":"+fmt.Sprint(appConfig.ServerPort)))
+	utils.LogExit(router.Run(appConfig.ServerHost+":"+"9998"))
 }
 
